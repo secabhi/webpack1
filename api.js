b62b3c03bda233820ac0d85247f6e917b71dc8e8
secabhi <abhiatstudy@gmail.com>
2017-06-21 21:56:31 +0530

    added

---
+++ api.js
@@ -0,0 +1,16 @@
+module.exports = {
+    getusers: function () {
+        return [{
+            name: "abhi",
+            age: "23"
+        },
+        {
+            name: "raja",
+            age: "26"
+        },
+        {
+            name: "rani",
+            age: "26"
+        }]
+    }
+}
